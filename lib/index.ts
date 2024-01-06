e7401152d3f1b6a529ec085b59f30e08b0e7f6fc
Jianan Sun <isunjn@gmail.com>
2024-01-06 23:33:27 +0800

    feat: consoo init

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,50 @@
-export function log(...args: any[]) {
+const cfg = {
+  noop: false,
+};
+
+type InitOption = {
+  noop?: boolean;
+  alias?: string | string[];
+  global?: boolean;
+};
+
+const init = (option?: InitOption) => {
+  const {
+    noop = false,
+    alias = [],
+    global = true,
+  } = option || {};
+
+  if (noop) {
+    cfg.noop = true;
+  }
+
+
+  if (global) {
+    const aliases = Array.isArray(alias) ? alias : [alias];
+    ["consoo", ...aliases].forEach(name => {
+      // @ts-expect-error
+      window[name] = window[name] || consoo;
+    });
+  }
+};
+
+const log = (...args: any[]) => {
+  if (cfg.noop) return;
   console.log(...args);
+};
+
+const clear = () => {
+  if (cfg.noop) return;
+  console.clear();
 }
+
+const consoo = {
+  init,
+  log,
+  clear,
+};
+
+export type Consoo = typeof consoo;
+
+export default consoo;
